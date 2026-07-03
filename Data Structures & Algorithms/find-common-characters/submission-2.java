f04a1255195718f9867f10632718259b59e3bb5c
Saarthak Gupta <saarthakgupta08@gmail.com>
2026-07-03 16:58:30 +0530

    Add: find-common-characters - submission-2

---
+++ Data Structures & Algorithms/find-common-characters/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        ArrayList<String> res = new ArrayList<>();
+        HashMap<Character, Integer> result = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            HashMap<Character, Integer> currCount = new HashMap<>();
+            for (Character c : word.toCharArray()) {
+                int cnt = currCount.computeIfAbsent(c, k -> 0);
+                currCount.put(c, ++cnt);
+            }
+                        System.out.println("currCount " + i);
+                        System.out.println(currCount);
+            for (var entry : currCount.entrySet()) {
+                result.put(entry.getKey(),
+                    i == 0 ? entry.getValue()
+                           : Math.min(entry.getValue(),
+                                 result.computeIfAbsent(entry.getKey(), (key) -> 0)));
+            }
+             for (var entry : result.entrySet()) {
+                result.put(entry.getKey(),
+                    i == 0 ? entry.getValue()
+                           : Math.min(entry.getValue(),
+                                 currCount.computeIfAbsent(entry.getKey(), (key) -> 0)));
+            }
+            System.out.println("result");
+            System.out.println(result);
+        }
+        for (var entry : result.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                res.add(entry.getKey().toString());
+            }
+        }
+        return res;
+    }
+}
